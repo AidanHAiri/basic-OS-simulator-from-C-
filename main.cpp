8e94ca1ef5a3405cacbf3c14f5b869d07a98ef28
AidanHAiri <haidaralkhoiri2008@gmail.com>
2025-12-21 20:34:19 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 using namespace std;
 
+class daftar{
+public:
+    string baseusrname[10];
+	string basepwd[10];
+    int i = 0;
+    char Loop;
+    
+    void akun(){
+        do {
+		    cout<<"masukkan  username anda:  ";
+		    cin >> baseusrname[i];
+		    cout << "masukkan password anda:  ";
+		    cin >> basepwd[i];
+		    i++;
+		    cout << "Buat akun baru?(y/n)  ";
+		    cin >> Loop;
+	    }
+    	while (i < 10 && Loop != 'n');
+    }
+};
 
 int main(){
 	int i = 0;
-	char Loop;
 	string usrnamelogin;
-	string baseusrname[10];
-	string basepwd[10];
+	daftar X;
 	string pwd;
+    X.akun();
 	
 	
-	do {
-		cout<<"masukkan  username anda:\n";
-		cin >> baseusrname[i];
-		cout << "masukkan password anda";
-		cin >> basepwd[i];
-		i++;
-		cout << "Tambah?";
-		cin >> Loop;
-	}
-	while (i < 10 && Loop != 'n');
 	
 	
-	cout << "Login\nmasukkan username anda";
+	cout << "Login\nmasukkan username anda:  ";
 	cin >> usrnamelogin;
-	cout << "masukkan password anda";
+	cout << "masukkan password anda:  ";
 	cin >> pwd;
 	
 	
 	bool verifikasi = false;
 	for (int i=0; i<10; i++){
-		if (usrnamelogin == baseusrname[i] && pwd == basepwd[i]){
+		if (usrnamelogin == X.baseusrname[i] && pwd == X.basepwd[i]){
 		verifikasi = true;
 		break;
 		} 
